09f7c1737de4b944bc75ff284df86c73385ca928
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-08-11 11:31:57 +0200

    clean syntax

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -2,22 +2,17 @@
 fetch('http://localhost:5678/api/works')
   .then(response => response.json())
   .then(works => {
-    // console.table(works)
+    console.table(works)
     showAllWorks(works)
   })
 
   .catch(function(error) {
     alert(error);
     })
-  // à chaque projet il crée une figure dans .gallery
 
+  // GOAL : à chaque projet il crée une figure dans .gallery
   function showAllWorks(works){
     let gallery = document.querySelector(".gallery")
-
-    // for (i=0;i<works.length;i++){
-    //     let work = document.createElement("figure")
-    //     gallery.dataset.id = works[i].id;
-    // }
 
     for (let work of works){
         // créer les nouveaux éléments
@@ -26,21 +21,14 @@ fetch('http://localhost:5678/api/works')
         let workFigCaption = document.createElement("figcaption")
         
         // inject the API data in the elements
-        gallery.dataset.id = work.id
-        // gallery.dataset.imageUrl = work.imageUrl
-        gallery.dataset.title = work.title
         workImg.src = work.imageUrl
         workImg.alt = work.title
         workFigCaption.innerText = work.title
 
-        // construct the nodes of the elemnts / lier au DOM
-
+        // construct the nodes of the elements / lier au DOM
         gallery.appendChild(workFigure)
         workFigure.appendChild(workImg)
         workFigure.appendChild(workFigCaption)
-
-        //récupérer les infos, créer l'élement, lui coller l'image et les infos, 
-        // append
     }
 
   }
