4e274390cbd0d50421ca2bea3b148073834ec8a1
Laurene Zyskind <laurene.zyskind@gmail.com>
2023-08-25 20:22:12 +0200

    init deleteWork (pas fini)

--- FrontEnd/js/script.js
+++ FrontEnd/js/script.js
@@ -157,3 +157,57 @@ modalProjet.addEventListener("click", (event) => {
     modales.close();
   });
 
+fetch('http://localhost:5678/api/works')
+.then(response => response.json())
+.then(works => {
+// console.table(works)
+showAllWorksModal(works)
+})
+
+.catch(function(error) {
+alert(error);
+})
+
+function showAllWorksModal(works){
+    for (let work of works){
+        let workFigureModale = document.createElement("figure")
+        let workImgModale = document.createElement("img") 
+        let btnIcons = document.createElement("div") 
+        let trash = document.createElement("button")
+        let move = document.createElement("button")
+
+        workFigureModale.id = work.id
+        workImgModale.src = work.imageUrl
+        workImgModale.alt = work.title
+        btnIcons.classList.add("btnIcons")
+        trash.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+        trash.classList.add("trash")
+        move.innerHTML = '<i class="fa-solid fa-up-down-left-right"></i>'
+        move.classList.add("move")
+ 
+
+        gallerieModale.appendChild(workFigureModale)
+        workFigureModale.appendChild(workImgModale)
+        workFigureModale.appendChild(btnIcons)
+        btnIcons.appendChild(move)
+        btnIcons.appendChild(trash)
+
+        trash.addEventListener("click", deleteWork)
+
+    } 
+}
+
+function deleteWork(e) {
+    let toDeleteElement = e.target.closest('figure')
+    let id = toDeleteElement.id
+    console.log(toDeleteElement);
+    fetch('http://localhost:5678/api/works/${id}', { 
+        method: 'DELETE', 
+        headers: {
+            'Autorization' : 'Bearer ${data}'
+        }
+    })
+    // .then(() => toDeleteElement.innerHTML = 'Delete successful');
+    .then(() => console.log("delete successful"))
+}
+
